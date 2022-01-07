85950072e1c36ad4a878566dfbb7bf967bdd4cd7
DavidLobato <david.lobato@gmail.com>
2022-01-06 23:55:02 -0300

    CRUD Scaffolding

---
+++ SaleswebMvc/Data/SaleswebMvcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SaleswebMvc.Models;
+
+namespace SaleswebMvc.Data
+{
+    public class SaleswebMvcContext : DbContext
+    {
+        public SaleswebMvcContext (DbContextOptions<SaleswebMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SaleswebMvc.Models.Department> Department { get; set; }
+    }
+}
